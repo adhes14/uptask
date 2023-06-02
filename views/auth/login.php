4d48e0c34304479d270bef3722f7778fc3762fab
adhes14 <adhemarduran@gmail.com>
2023-06-02 13:49:25 -0400

    create html for login page

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,1 +1,24 @@
-<h1>Login</h1>
+<div class="contenedor">
+  <h1>UpTask</h1>
+  <p>Create and manage your projects</p>
+
+  <div class="contenedor-sm">
+    <p class="descripcion-pagina">Log In</p>
+    <form action="/" class="formulario" method="POST">
+      <div class="campo">
+        <label for="email">Email</label>
+        <input type="email" id="email" placeholder="Your email" name="email">
+      </div>
+      <div class="campo">
+        <label for="password">Password</label>
+        <input type="password" id="password" placeholder="Your password" name="password">
+      </div>
+      <input type="submit" class="boton" value="Log In">
+    </form>
+
+    <div class="acciones">
+      <a href="/create">Create an account</a>
+      <a href="/forgot">Recover your password</a>
+    </div>
+  </div>
+</div>
