223ed1953a215616b91ca0fe02a3f3238b6f2169
adhes14 <adhemarduran@gmail.com>
2023-06-22 11:06:00 -0400

    add task button in project page

--- views/dashboard/project.php
+++ views/dashboard/project.php
@@ -1,5 +1,11 @@
 <?php include_once __DIR__ . '/header.php'; ?>
 
-
+  <div class="contenedor-sm">
+    <div class="contenedor-nueva-tarea">
+      <button class="agregar-tarea" type="button" id="agregar-tarea">
+        &#43; New task
+      </button>
+    </div>
+  </div>
 
 <?php include_once __DIR__ . '/footer.php'; ?>
