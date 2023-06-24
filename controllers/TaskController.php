ceb15af574ef41dfc5e2689cfde39bce10fb4ce5
adhes14 <adhemarduran@gmail.com>
2023-06-24 14:51:54 -0400

    create TaskController

---
+++ controllers/TaskController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Controllers;
+
+class TaskController {
+  public static function index() {
+    
+  }
+
+  public static function create() {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+      
+    }
+  }
+
+  public static function update() {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    }
+  }
+
+  public static function delete() {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    }
+  }
+}
--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/../includes/app.php';
 
 use Controllers\DashboardController;
 use Controllers\LoginController;
+use Controllers\TaskController;
 use MVC\Router;
 $router = new Router();
 
@@ -34,6 +35,13 @@ $router->get('/create-project', [DashboardController::class, 'create_project']);
 $router->post('/create-project', [DashboardController::class, 'create_project']);
 $router->get('/project', [DashboardController::class, 'project']);
 $router->get('/profile', [DashboardController::class, 'profile']);
+
+// API for tasks
+$router->get('/api/tasks', [TaskController::class, 'index']);
+$router->post('/api/task', [TaskController::class, 'create']);
+$router->post('/api/task/update', [TaskController::class, 'update']);
+$router->post('/api/task/delete', [TaskController::class, 'delete']);
+
 
 // Check if paths exists and give them controllers' methods
 $router->comprobarRutas();
